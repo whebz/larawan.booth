934916d743832028271a141589d15896139abbcc
grace.villamarin <grace.villamarin@retailitaliasrl.onmicrosoft.com>
2023-12-31 02:09:55 +0100

    changed imaged

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-(function(){
+(function () {
 
   'use strict';
 
   var canvas = document.getElementById('scratch'),
-      context = canvas.getContext('2d');
+    context = canvas.getContext('2d'),
+    container = document.getElementById('container');
 
   // default value
   context.globalCompositeOperation = 'source-over';
 
   //----------------------------------------------------------------------------
 
-var x = 0;
-var y = 0;
+  var x = 0;
+  var y = 0;
 
   // fill circle
   context.beginPath();
   context.fillStyle = '#b8964f';
-  context.fillRect(0, 0, 197, 165);
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-  context.font="20px system-ui";
-  context.textAlign="center"; 
+  context.font = "20px system-ui";
+  context.textAlign = "center";
   context.textBaseline = "middle";
   context.fillStyle = "#ffffff";
-  var rectHeight = 165;
-  var rectWidth = 197;
-  var rectX = 10;
-  var rectY = 10;
-  context.fillText("Scratch here!",rectWidth/2,rectHeight/2);
+  context.fillText("Scratch here!", canvas.width / 2, canvas.height / 2);
 
   //----------------------------------------------------------------------------
 
@@ -35,18 +32,18 @@ var y = 0;
   function clearArc(x, y) {
     context.globalCompositeOperation = 'destination-out';
     context.beginPath();
-    context.fillRect(x, y, 10, 10);
+    context.fillRect(x, y, 20, 40);
     // context.arc(x, y, 10, 0, Math.PI * 2, false);
     // context.fill();
   }
 
-  canvas.addEventListener('mousedown', function(event) {
+  canvas.addEventListener('mousedown', function (event) {
     isDrag = true;
 
     clearArc(event.offsetX, event.offsetY);
   }, false);
 
-  canvas.addEventListener('mousemove', function(event) {
+  canvas.addEventListener('mousemove', function (event) {
     if (!isDrag) {
       return;
     }
@@ -54,23 +51,23 @@ var y = 0;
     clearArc(event.offsetX, event.offsetY);
   }, false);
 
-  canvas.addEventListener('mouseup', function(event) {
+  canvas.addEventListener('mouseup', function (event) {
     isDrag = false;
   }, false);
 
-  canvas.addEventListener('mouseleave', function(event) {
+  canvas.addEventListener('mouseleave', function (event) {
     isDrag = false;
   }, false);
 
   //----------------------------------------------------------------------------
 
-  canvas.addEventListener('touchstart', function(event) {
+  canvas.addEventListener('touchstart', function (event) {
     if (event.targetTouches.length !== 1) {
       return;
     }
     let pos = { x: event.clientX, y: event.clientY };
     if (event.touches) {
-        pos = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+      pos = { x: event.touches[0].clientX, y: event.touches[0].clientY };
     }
     const rect = event.target.getBoundingClientRect();
     const x_rel = pos.x - rect.left;
@@ -85,14 +82,14 @@ var y = 0;
     clearArc(x, y);
   }, false);
 
-  canvas.addEventListener('touchmove', function(event) {
+  canvas.addEventListener('touchmove', function (event) {
     if (!isDrag || event.targetTouches.length !== 1) {
       return;
     }
 
     let pos = { x: event.clientX, y: event.clientY };
     if (event.touches) {
-        pos = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+      pos = { x: event.touches[0].clientX, y: event.touches[0].clientY };
     }
     const rect = event.target.getBoundingClientRect();
     const x_rel = pos.x - rect.left;
@@ -105,7 +102,7 @@ var y = 0;
     clearArc(x, y);
   }, false);
 
-  canvas.addEventListener('touchend', function(event) {
+  canvas.addEventListener('touchend', function (event) {
     isDrag = false;
   }, false);
 
